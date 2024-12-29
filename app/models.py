1c750ceee6d7dc79db7b070513f192419c9b91ce
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2024-12-29 23:32:34 +0700

    Added Bootstrap

--- app/models.py
+++ app/models.py
@@ -2,11 +2,13 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
-
+from time import time
+import jwt
+from app import app
 
 followers = sa.Table(
     'followers',
@@ -72,7 +74,6 @@ class User(UserMixin, db.Model):
             self.following.select().subquery())
         return db.session.scalar(query)
 
-
     def following_posts(self):
         Author = so.aliased(User)
         Follower = so.aliased(User)
@@ -87,6 +88,20 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
